981f00c6986e4b8d18d97d885d858b02ea71f114
mcPython04 <chen1911893611@gmail.com>
2022-08-02 14:17:34 -0500

    added another solution

--- trees/trees.py
+++ trees/trees.py
@@ -1,4 +1,4 @@
-# RECURSIVE DFS
+# RECURSIVE DFS (104)
 def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
@@ -6,7 +6,7 @@ def maxDepth(self, root: TreeNode) -> int:
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
-# ITERATIVE DFS
+# ITERATIVE DFS (104)
 def maxDepth1(self, root: Treenode) -> int:
 	stack = [[root, 1]]
 	res = 0
@@ -21,7 +21,7 @@ def maxDepth1(self, root: Treenode) -> int:
 	return res
 
 
-# ITERATIVE BFS
+# ITERATIVE BFS (104)
 def maxDepth2(self, root: Treenode) -> int:
 	if not root:
 		return 0
@@ -38,3 +38,29 @@ def maxDepth2(self, root: Treenode) -> int:
                     q.append(node.right)
             level += 1
         return level
+
+
+# Diameter of Binary Tree (543)
+# Time : O(n)
+def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        # Setup global variable
+        res = [0]
+        
+        
+        # Setup recursive DFS 
+        def dfs(root):
+            if not root:
+                return -1
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            # Alg that calculates diameter
+            res[0] = max(res[0], 2 + left + right)
+            
+            # Returns the height of the current node
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return res[0]
